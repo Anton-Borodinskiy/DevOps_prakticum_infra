f6c37ade0cdd13324a7fdf9ceea84269d1627a30
Антон Бородинский <borodinskiy26@yandex.ru>
2022-06-05 22:50:23 +0000

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -8,6 +8,6 @@ curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.tar.gz
 
 sudo mkdir -p /tmp/sausage-store-frontend-${VERSION}
 sudo tar -xzvf sausage-store-${VERSION}.tar.gz -C /tmp/sausage-store-frontend-${VERSION}
-sudo cp -rf /tmp/sausage-store-front-${VERSION}/sausage-store-${VERSION}/public_html/. /var/www-data/dist/frontend/
+sudo cp -rf /tmp/sausage-store-frontend-${VERSION}/sausage-store-${VERSION}/public_html/. /var/www-data/dist/frontend/
 sudo systemctl daemon-reload
 sudo systemctl restart sausage-store-frontend

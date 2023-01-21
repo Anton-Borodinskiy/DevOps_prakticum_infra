94edb752a4d57f966ec8e7fbfba8a3731dfb087b
borodinskiy <anton.borodinskiy@mail.ru>
2023-01-22 01:53:56 +0300

    Docker_lection

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,14 +1,19 @@
-#! /bin/bash
-#Если свалится одна из команд, рухнет и весь скрипт
-set -xe
-#Перезаливаем дескриптор сервиса на ВМ для деплоя
-sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
-sudo rm -f /home/jarservice/sausage-store.jar||true
-#Переносим артефакт в нужную папку
-echo -e "PSQL_DB=${PSQL_DB}\nPSQL_USER=${PSQL_USER}\nPSQL_PASS=${PSQL_PASS}\nMONGO_PASS=${MONGO_PASS}\nMONGO_USER=${MONGO_USER}\nMONGO_HOST=${MONGO_HOST}\nMONGO_DB=${MONGO_DB}" | sudo tee /home/jarservice/environment
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
-sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true #"jar||true" говорит, если команда обвалится — продолжай
-#Обновляем конфиг systemd с помощью рестарта
-sudo systemctl daemon-reload
-#Перезапускаем сервис сосисочной
-sudo systemctl restart sausage-store-backend
+#!/bin/bash
+set +e
+cat > .env <<EOF
+SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}
+SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
+SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
+SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
+EOF
+docker network create -d bridge sausage_network || true
+docker pull gitlab.praktikum-services.ru:5050/anton-borodinskiy/sausage-store/sausage-backend:latest
+docker stop backend || true
+docker rm backend || true
+set -e
+docker run -d --name backend \
+    --network=sausage_network \
+    --restart always \
+    --pull always \
+    --env-file .env \
+    gitlab.praktikum-services.ru:5050/anton-borodinskiy/sausage-store/sausage-backend:latest

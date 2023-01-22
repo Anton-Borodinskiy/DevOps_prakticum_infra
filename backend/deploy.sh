21465deb82be145b1ded2181239c9c5516ca24a3
borodinskiy <anton.borodinskiy@mail.ru>
2023-01-22 03:43:48 +0300

    Docker_lection5

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -5,13 +5,14 @@ SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}
 SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
+SPRING_DATA_MONGODB=${SPRING_DATA_MONGODB_URI}
 EOF
 docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/anton-borodinskiy/sausage-store/sausage-backend:latest
-docker stop backend || true
-docker rm backend || true
+docker stop sausage-backend || true
+docker rm sausage-backend || true
 set -e
-docker run -d --name backend \
+docker run -d --name sausage-backend \
     --network=sausage_network \
     --restart always \
     --pull always \

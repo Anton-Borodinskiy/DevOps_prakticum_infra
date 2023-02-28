2c3efa6e39405b8d96489c0a634149e27dd7f742
borodinskiy <anton.borodinskiy@mail.ru>
2023-02-28 03:03:37 +0300

    deploy3

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,81 +1,47 @@
 #!/bin/bash
-is-container-up() {
-    local container=${1?"Usage: ${FUNCNAME[0]} container_name"}
+set +e
 
-    [ -n "$(docker --context remote ps -f name=${container} -q)" ]
-    return $?
-}
+sudo docker login ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
+sudo docker network create -d bridge sausage-store || true
+docker-compose pull
 
-get-active-slot() {
-    local image=${1?"Usage: ${FUNCNAME[0]} image_name"}
-
-    if is-container-up ${image}-blue && is-container-up ${image}-green; then
-        echo "Collision detected! Stopping ${image}-green..."
-        docker-compose -H ssh://${DEV_USER}@${DEV_HOST} rm -s -f backend-green
-        return 0  # BLUE
-    fi
-    if is-container-up ${image}-blue && ! is-container-up ${image}-green; then
-        return 0  # BLUE
-    fi
-    if ! is-container-up ${image}-blue; then
-        return 1  # GREEN
-    fi
-}
-
-get-service-status() {
-    local usage_msg="Usage: ${FUNCNAME[0]} image_name deployment_slot"
-    local image=${1?usage_msg}
-    local slot=${2?$usage_msg}
-
-    case $image in
-        # Add specific healthcheck paths for your services here
-        *) local health_check_port_path=":8080/actuator/health" ;;
-    esac
-    local health_check_address="http://${image}-${slot}${health_check_port_path}"
-    echo "Requesting '$health_check_address' within the 'sausage-store_sausage-store' docker network:"
-    docker --context remote run --rm --network sausage-store_sausage-store alpine \
-        wget --timeout=1 --quiet --server-response $health_check_address
-    return $?
-}
-
-deploy() {
-    local image_name=${1?"Usage: ${FUNCNAME[0]} image_name"}
-
-    if get-active-slot $image_name
-    then
-        local OLD=${image_name}-blue
-        local old_slot=blue
-        local new_slot=green
-    else
-        local OLD=${image_name}-green
-        local new_slot=blue
-        local old_slot=green
-    fi
-    local NEW=${image_name}-${new_slot}
-    echo "Ensuring the '$NEW' container from previous deploy is removed..."
-    docker-compose -H ssh://${DEV_USER}@${DEV_HOST} rm -s -f backend-${new_slot} || :
-    echo "Deploying '$NEW' in place of '$OLD'..."
-    docker-compose -H ssh://${DEV_USER}@${DEV_HOST} up -d --force-recreate backend-${new_slot}
-    echo "Container started. Checking health..."
-    for i in {1..20}
+if [[ $(docker container inspect -f '{{.State.Running}}' backend-blue) = true ]]
+then
+    echo "Blue continer is now running"
+    docker-compose up -d --force-recreate backend-green
+    green_status=$(docker container inspect -f '{{.State.Health.Status}}' backend-green)
+    until [ "$green_status" = "healthy" ]
     do
-        sleep 1
-        if get-service-status backend $new_slot
-        then
-            echo "New '$NEW' service seems OK."
-            sleep 2  # Ensure service is ready
-            echo "The '$NEW' service is live!"
-            sleep 2  # Ensure all requests were processed
-            echo "Stopping '$OLD'..."
-            docker-compose -H ssh://${DEV_USER}@${DEV_HOST} rm -s -f backend-${old_slot}
-            echo "Deployment successful!"
-            return 0
-        fi
-        echo "New '$NEW' service is not ready yet. Waiting ($i)..."
+      green_status=$(docker container inspect -f '{{.State.Health.Status}}' backend-green)
+      echo $green_status
+      sleep 10
     done
-    echo "New '$NEW' service did not raise, killing it. Failed to deploy T_T"
-    docker-compose -H ssh://${DEV_USER}@${DEV_HOST} rm -s -f backend-${new_slot}
-    return 5
-}
+    echo "Green container is up!"
+    docker stop backend-blue
 
-deploy sausage-backend
+elif [[ $(docker container inspect -f '{{.State.Running}}' backend-green) = true ]]
+then
+    echo "Green contianer is now running"
+    docker-compose up -d --force-recreate backend-blue
+    blueStatus=$(docker container inspect -f '{{.State.Health.Status}}' backend-blue)
+    until [ "$blue_status" = "healthy" ]
+    do
+      blueStatus=$(docker container inspect -f '{{.State.Health.Status}}' backend-blue)
+      echo $blue_status
+      sleep 10
+    done
+    echo "Blue container is up!"
+    docker stop backend-green
+else
+    # Starting blue if all container is inactive
+    echo "All backend containers is inacive. Start blue..."
+    docker-compose up -d --force-recreate backend-blue
+    blueStatus=$(docker container inspect -f '{{.State.Health.Status}}' backend-blue)
+    until [ "$blue_status" = "healthy" ]
+    do
+      blueStatus=$(docker container inspect -f '{{.State.Health.Status}}' backend-blue)
+      echo $blue_status
+      sleep 10
+    done
+    echo "Blue container is up!"
+fi
